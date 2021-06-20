897596def6077dc645d789546106387c25e98923
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2021-06-20 08:31:57 +0200

    ss

--- app/src/main/java/pl/training/goodweather/MainActivity.kt
+++ app/src/main/java/pl/training/goodweather/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import pl.training.goodweather.GoodWeatherApplication.Companion.applicationGraph
 import pl.training.goodweather.common.UserPreferences
 import pl.training.goodweather.databinding.ActivityMainBinding
@@ -22,25 +24,27 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private val db = Firebase.firestore
 
     @Inject
     lateinit var userPreferences: UserPreferences
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        applicationGraph.inject(this)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        navView.setupWithNavController(navController)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            applicationGraph.inject(this)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            setSupportActionBar(binding.toolbar)
+            val navController = findNavController(R.id.nav_host_fragment_content_main)
+            appBarConfiguration = AppBarConfiguration(navController.graph)
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            val navView: BottomNavigationView = findViewById(R.id.nav_view)
+            navView.setupWithNavController(navController)
 
-        PreferenceManager.getDefaultSharedPreferences(this)
-            .registerOnSharedPreferenceChangeListener(userPreferences)
-    }
+            PreferenceManager.getDefaultSharedPreferences(this)
+                .registerOnSharedPreferenceChangeListener(userPreferences)
+            firebaseExample()
+        }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -60,6 +64,68 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun firebaseExample() {
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("###", "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w("###", "Error adding document", e)
+            }
+
+        db.collection("users")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("###", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("###", "Error getting documents.", exception)
+            }
+
+        db.collection("users").whereEqualTo("first", "Ada")
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d("###", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("###", "Error getting documents: ", exception)
+            }
+
+        db.collection("users").document("1wBtF9ijsciZgsRCfvoa").addSnapshotListener { snapshot, e ->
+            if (e != null) {
+                Log.w("###", "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            val source = if (snapshot != null && snapshot.metadata.hasPendingWrites())
+                "Local"
+            else
+                "Server"
+
+            if (snapshot != null && snapshot.exists()) {
+                Log.d("###", "$source data: ${snapshot.data}")
+            } else {
+                Log.d("###", "$source data: null")
+            }
+        }
+
+        db.collection("users").document("1wBtF9ijsciZgsRCfvoa")
+            .delete()
+            .addOnSuccessListener { Log.d("###", "DocumentSnapshot successfully deleted!") }
+            .addOnFailureListener { e -> Log.w("###", "Error deleting document", e) }
     }
 
 }
