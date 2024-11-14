511d9ba60ff480e93ad2206a476c506f7341baab
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-11-14 15:44:27 +0100

    UI

--- app/src/main/java/pl/training/runkeeper/weather/adapters/view/ForecastActivity.kt
+++ app/src/main/java/pl/training/runkeeper/weather/adapters/view/ForecastActivity.kt
@@ -10,7 +10,7 @@ class ForecastActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_forecast)
         initView()
     }
 
