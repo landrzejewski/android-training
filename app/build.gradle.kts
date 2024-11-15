e910b250f2c37b3ae032034b7b0e187240e1c77d
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2024-11-15 10:23:18 +0100

    UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
--- app/src/main/java/pl/training/runkeeper/weather/adapters/view/ForecastViewModel.kt
+++ app/src/main/java/pl/training/runkeeper/weather/adapters/view/ForecastViewModel.kt
@@ -2,13 +2,31 @@ package pl.training.runkeeper.weather.adapters.view
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import pl.training.runkeeper.common.formatDate
+import pl.training.runkeeper.common.formatPressure
+import pl.training.runkeeper.common.formatTemperature
 import pl.training.runkeeper.weather.WeatherConfiguration
+import pl.training.runkeeper.weather.domain.DayForecast
 
-class ForecastViewModel {
+class ForecastViewModel : ViewModel() {
 
     private val forecastService = WeatherConfiguration.getForecastService()
     private val forecastData = MutableLiveData<List<DayForecastViewModel>>()
 
     val forecast: LiveData<List<DayForecastViewModel>> = forecastData
+
+    fun refreshForecast(city: String) {
+        viewModelScope.launch {
+            val data = forecastService.getForecast(city).map(::toViewModel)
+            forecastData.postValue(data)
+        }
+    }
+
+    private fun toViewModel(dayForecast: DayForecast) = with(dayForecast) {
+        DayForecastViewModel(formatDate(date), formatTemperature(temperature), formatPressure(pressure), description, iconName)
+    }
 
 }
