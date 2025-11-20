35cc6a3ea2ed0387719e916332d089d3b6b2fc4e
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-11-20 15:42:22 +0100

    Http

--- app/src/main/java/pl/training/runkeeper/weather/WeatherConfiguration.kt
+++ app/src/main/java/pl/training/runkeeper/weather/WeatherConfiguration.kt
@@ -5,6 +5,8 @@ import pl.training.runkeeper.weather.domain.ForecastService
 
 object WeatherConfiguration {
 
+    // https://api.openweathermap.org/data/2.5/forecast/daily?units=metric&appid=b933866e6489f58987b2898c89f542b8&q=warsaw
+
     val forecastService by lazy { ForecastService(FakeForecastProvider()) }
 
 }
