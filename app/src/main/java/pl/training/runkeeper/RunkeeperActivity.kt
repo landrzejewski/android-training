e03ab31b39ecba24f83da110afa770c91d2d1fe6
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-11-27 11:40:42 +0100

    hilt

---
+++ app/src/main/java/pl/training/runkeeper/RunkeeperActivity.kt
@@ -0,0 +1,35 @@
+package pl.training.runkeeper
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+import pl.training.runkeeper.databinding.ActivityRunkeeperBinding
+
+@AndroidEntryPoint
+class RunkeeperActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRunkeeperBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRunkeeperBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp() = navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+
+}
