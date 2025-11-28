62044b8f003cd6af1c233a82424c6a8367717278
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2025-11-28 23:39:45 +0100

    Android components

--- app/src/main/java/pl/training/runkeeper/RunkeeperActivity.kt
+++ app/src/main/java/pl/training/runkeeper/RunkeeperActivity.kt
@@ -1,6 +1,11 @@
 package pl.training.runkeeper
 
+import android.content.Intent.ACTION_AIRPLANE_MODE_CHANGED
+import android.content.IntentFilter
 import android.os.Bundle
+import android.os.Environment.MEDIA_MOUNTED
+import android.os.Environment.MEDIA_MOUNTED_READ_ONLY
+import android.os.Environment.getExternalStorageState
 import android.view.Menu
 import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
@@ -13,6 +18,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import pl.training.runkeeper.common.components.AirplaneModeReceiver
 import pl.training.runkeeper.databinding.ActivityRunkeeperBinding
 
 @AndroidEntryPoint
@@ -21,6 +27,8 @@ class RunkeeperActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRunkeeperBinding
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private val airplaneModeReceiver = AirplaneModeReceiver()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,5 +61,76 @@ class RunkeeperActivity : AppCompatActivity() {
         }
         else -> super.onOptionsItemSelected(item)
     }
+
+    private fun componentExamples() {
+        // Broadcast receiver
+
+        val airplaneModeIntent = IntentFilter(ACTION_AIRPLANE_MODE_CHANGED)
+        registerReceiver(airplaneModeReceiver, airplaneModeIntent)
+        // unregisterReceiver(airplaneModeReceiver)
+
+        // Content provider
+
+        /*val values = ContentValues()
+        values.put(DATE_COLUMN, Date().time)
+        values.put(CONDITIONS_COLUMN, "Sunny")
+
+        val uri = contentResolver.insert(CONTENT_URI, values)
+        Log.i("###", uri.toString())*/
+
+        /* val queryUri = Uri.parse("content://pl.training.runkeeper.common.components.ForecastProvider/forecast")
+         contentResolver.query(queryUri, null, null, null)
+             ?.let {
+                 val dateColumnIdx = it.getColumnIndex(DATE_COLUMN)
+                 val conditionsIdx = it.getColumnIndex(CONDITIONS_COLUMN)
+                 while (it.moveToNext()) {
+                     val date = Date(it.getLong(dateColumnIdx))
+                     val conditions = it.getString(conditionsIdx)
+                     Log.i("###", "$date:$conditions")
+                 }
+                 it.close()
+             }*/
+
+        // Service
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(Intent(this, SchedulerService::class.java))
+        } else {
+            startService(Intent(this, SchedulerService::class.java))
+        }*/
+
+
+        // App file storage
+
+        /*openFileOutput("data.txt", MODE_PRIVATE).use {
+            it.write("To jest text".toByteArray())
+        }
+
+        openFileInput("data.txt").bufferedReader()
+            .forEachLine {
+                Log.i("###", it)
+            }
+
+        // External file storage
+        if (externalStorageExists() && externalStorageWritable()) {
+            val path = File(getExternalFilesDir("Data"), "app_data.txt").toPath()
+            Log.i("###", path.toString())
+            Files.write(path, "Hello".toByteArray())
+            Files.readAllLines(path).forEach {
+                Log.i("###", it)
+            }
+        }*/
+
+    }
+
+    private fun externalStorageExists() = MEDIA_MOUNTED == getExternalStorageState()
+
+    private fun externalStorageWritable() = MEDIA_MOUNTED_READ_ONLY != getExternalStorageState()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(airplaneModeReceiver)
+        // stopService(Intent(this, SchedulerService::class.java))
+    }
+
 
 }
---
+++ app/src/main/java/pl/training/runkeeper/common/components/SchedulerService.kt
@@ -0,0 +1,85 @@
+package pl.training.runkeeper.common.components
+
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.app.Notification
+import android.app.Notification.CATEGORY_SERVICE
+import android.app.Notification.VISIBILITY_PRIVATE
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_NONE
+import android.app.Service
+import android.content.Intent
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.graphics.Color
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.ActivityCompat.checkSelfPermission
+import androidx.core.app.NotificationManagerCompat
+import pl.training.runkeeper.R
+import java.util.Timer
+import java.util.TimerTask
+
+class SchedulerService : Service() {
+
+    private var counter = 100
+    private val timer = Timer()
+    private val timerTask: TimerTask = object : TimerTask() {
+
+        override fun run() {
+            counter--
+            Log.i("###", "Time to execution ${counter}s")
+            if (counter <= 0) {
+                timer.cancel()
+                Log.i("###", "Executing task")
+            }
+        }
+
+    }
+
+    override fun onBind(intent: Intent?): IBinder? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        init()
+        Log.i("###", "SchedulerService started")
+    }
+
+    private fun init() {
+        val thisContext = this
+        with(NotificationManagerCompat.from(this)) {
+            if (checkSelfPermission(thisContext, POST_NOTIFICATIONS) != PERMISSION_GRANTED) {
+                return
+            }
+            notify(ID, createNotification())
+        }
+    }
+
+    private fun createNotification(): Notification {
+        val channel = NotificationChannel(CLIENT_ID, CHANNEL_NAME, IMPORTANCE_NONE)
+        channel.lightColor = Color.RED
+        channel.lockscreenVisibility = VISIBILITY_PRIVATE
+        val manager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        manager.createNotificationChannel(channel)
+        return Notification.Builder(this, CLIENT_ID)
+            .setOngoing(true)
+            .setContentTitle("Task is scheduled")
+            .setSmallIcon(R.drawable.ic_sun)
+            .setCategory(CATEGORY_SERVICE)
+            .build()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        super.onStartCommand(intent, flags, startId)
+        timer.schedule(timerTask, 0, 1_000)
+        return START_STICKY
+    }
+
+    companion object {
+
+        private const val CLIENT_ID = "pl.training.runkeeper"
+        private const val CHANNEL_NAME = "scheduling"
+        private const val ID = 1
+
+    }
+
+}
